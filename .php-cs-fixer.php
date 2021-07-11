8b21d1eba9f2a9f1d000993af8d0ca06d12f5f9b
Adrian Pop <adrian.pop@bitpanda.com>
2021-07-11 22:27:46 +0300

    Add php-cs fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -14,7 +14,7 @@ return $config->setRules([
         'no_unused_imports' => true,
         'array_syntax' => ['syntax' => 'short'],
         'not_operator_with_successor_space' => true,
-        'trailing_comma_in_multiline_array' => true,
+        'trailing_comma_in_multiline' => true,
         'phpdoc_scalar' => true,
         'unary_operator_spaces' => true,
         'binary_operator_spaces' => true,
